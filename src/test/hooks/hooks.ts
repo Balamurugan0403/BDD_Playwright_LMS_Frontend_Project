1a588413478ad93f80e7e7acacaa9d3372177c93
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-07-16 11:52:04 +0530

    Enhanced the project structure

--- src/test/hooks/hooks.ts
+++ src/test/hooks/hooks.ts
@@ -1,20 +1,10 @@
 import { Before, After, BeforeAll, AfterAll } from '@cucumber/cucumber';
 import { chromium, Browser, firefox, webkit } from '@playwright/test';
 import { CustomWorld } from '../../main/support/CustomWorld';
-import { LoginPage } from '../pages/LoginPage';
-import { CourseStructurePage } from '../pages/CourseStructurePage';
-import { CourseCategoryPage } from '../pages/CourseCategoryPage';
-import { DynamicFieldManagementPage } from '../pages/DynamicFieldManagementPage';
-import { config } from '../../main/config/config';
+import { config } from '../../main/config/Config';
 import { SidebarPage } from '../pages/SidebarPage';
-import { ServiceModelPage } from '../pages/ServiceModelPage';
-import { AddCoursePage } from '../pages/AddCoursePage';
-import { SearchCoursePage } from '../pages/SearchCoursePage';
-import { CourseFilterPage } from "../../test/pages/CourseFilterPage";
-import { EditCoursePage } from '../pages/EditCoursePage';
-import { PedagogyPage } from "../../test/pages/PedagogyPage";
-
-import { logger } from '../../main/utils/logger';
+import { logger } from '../../main/utils/Logger';
+import { TrainingSummaryPage } from '../pages/TrainingSummaryPage';
 
 let browser: Browser;
 BeforeAll(async () => {
@@ -40,19 +30,8 @@ Before(async function (this: CustomWorld, scenario) {
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
 
-    this.loginPage = new LoginPage(this.page);
-    this.courseStructurePage = new CourseStructurePage(this.page);
-    this.courseCategoryPage = new CourseCategoryPage(this.page);
-    this.dynamicFieldManagementPage = new DynamicFieldManagementPage(this.page);
-    this.addCoursePage = new AddCoursePage(this.page);
     this.sidebarPage = new SidebarPage(this.page);
-    this.serviceModelPage = new ServiceModelPage(this.page)
-    this.courseStructurePage = new CourseStructurePage(this.page);
-    this.addCoursePage = new AddCoursePage(this.page);
-    this.searchCoursePage = new SearchCoursePage(this.page);
-    this.courseFilterPage = new CourseFilterPage(this.page);
-    this.editCoursePage = new EditCoursePage(this.page);
-    this.pedagogyPage=new PedagogyPage(this.page);
+    this.trainingSummaryPage = new TrainingSummaryPage(this.page);
 });
 
 After(async function (this: CustomWorld, scenario) {
