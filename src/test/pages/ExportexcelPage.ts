70367d62acf043f3a1b79e2b7de79237dea44674
Balamurugan <murugan020325@gmail.com>
2026-07-21 07:14:49 +0530

    Add logging to Export Excel page

--- src/test/pages/ExportexcelPage.ts
+++ src/test/pages/ExportexcelPage.ts
@@ -1,11 +1,14 @@
 import { BasePage } from "./BasePage";
+import { logger } from "../../main/utils/logger";
 
 export class ExportexcelPage extends BasePage {
     private readonly exportButton = this.page.getByRole("button", {name: "EXPORT TO EXCEL"});
 
     async exportEmployeeDetails() {
+        logger.info("Clicking Export to Excel button");
         const downloadEvent = this.page.waitForEvent("download");
         await this.click(this.exportButton);
+        logger.info("Excel file downloaded successfully");
         return await downloadEvent;
     }
 }
