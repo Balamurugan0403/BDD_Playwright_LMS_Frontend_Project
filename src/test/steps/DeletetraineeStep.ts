59adeda170038f68adffa168a5e1b0e9ce4f832d
SOWNDARIYA K <146727613+sowndariya-k@users.noreply.github.com>
2026-08-10 16:25:30 +0530

    Add the trainee step with timeout

--- src/test/steps/DeletetraineeStep.ts
+++ src/test/steps/DeletetraineeStep.ts
@@ -1,12 +1,13 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { CustomWorld } from "../../main/support/CustomWorld";
 
-// NOTE: "user is on the homepage of the site" is already defined
-// in AddTraineeStep.ts, so it is NOT repeated here.
-
-Given("user searches for the trainee {string} in the filter", async function (this: CustomWorld, empId: string) {
-    await this.deleteTraineePage.searchTrainee(empId);
-});
+Given(
+    "user searches for the trainee {string} in the filter",
+    { timeout: 15000 },
+    async function (this: CustomWorld, empId: string) {
+        await this.deleteTraineePage.searchTrainee(empId);
+    }
+);
 
 Then("user should see the trainee {string} in the filtered list", async function (this: CustomWorld, empId: string) {
     await this.deleteTraineePage.verifyTraineeVisible(empId);
@@ -24,9 +25,13 @@ Then("the trainee {string} should be deleted successfully", async function (this
     await this.deleteTraineePage.verifyDeleteSuccessful();
 });
 
-When("user searches again for the trainee {string} in the filter", async function (this: CustomWorld, empId: string) {
-    await this.deleteTraineePage.searchTrainee(empId);
-});
+When(
+    "user searches again for the trainee {string} in the filter",
+    { timeout: 15000 },
+    async function (this: CustomWorld, empId: string) {
+        await this.deleteTraineePage.searchTrainee(empId);
+    }
+);
 
 Then("the trainee {string} should not be shown in the list", async function (this: CustomWorld, empId: string) {
     await this.deleteTraineePage.verifyTraineeNotVisible(empId);
